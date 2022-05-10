332d3cc8fb953d6052b676e50e5aa34d220379bc
trung-nt3 <trungqn.ptit@gmail.com>
2022-05-10 16:50:51 +0700

    init nix

---
+++ default.nix
@@ -0,0 +1,49 @@
+{ lib
+, buildGoApplication
+, nix-gitignore
+, rocksdb ? null
+, db_backend ? "rocksdb"
+, network ? "mainnet"  # mainnet|testnet
+, rev ? "dirty"
+}:
+let
+  version = "v0.1.0";
+  pname = "astrad";
+  tags = lib.concatStringsSep "," (
+    [ "ledger" "cgo" network ]
+    ++ lib.lists.optionals (db_backend == "rocksdb") [ "rocksdb" ]
+  );
+  ldflags = lib.concatStringsSep "\n" ([
+    "-X github.com/cosmos/cosmos-sdk/version.Name=astra"
+    "-X github.com/cosmos/cosmos-sdk/version.AppName=${pname}"
+    "-X github.com/cosmos/cosmos-sdk/version.Version=${version}"
+    "-X github.com/cosmos/cosmos-sdk/version.BuildTags=${tags}"
+    "-X github.com/cosmos/cosmos-sdk/version.Commit=${rev}"
+  ] ++ lib.lists.optionals (db_backend == "rocksdb") [
+    "-X github.com/cosmos/cosmos-sdk/types.DBBackend=rocksdb"
+  ]);
+  buildInputs = lib.lists.optionals (db_backend == "rocksdb") [
+    rocksdb
+  ];
+in
+buildGoApplication rec {
+  inherit pname version buildInputs;
+  src = (nix-gitignore.gitignoreSourcePure [
+    "/*" # ignore all, then add whitelists
+    "!/x/"
+    "!/app/"
+    "!/cmd/"
+    "!/client/"
+    "!go.mod"
+    "!go.sum"
+  ] ./.);
+  modules = ./gomod2nix.toml;
+  pwd = src; # needed to support replace
+  subPackages = [ "cmd/astrad" ];
+  CGO_ENABLED = "1";
+  buildFlags = "-tags=${tags}";
+  buildFlagsArray = ''
+    -ldflags=
+    ${ldflags}
+  '';
+}
