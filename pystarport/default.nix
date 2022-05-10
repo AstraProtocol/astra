5964e2b18ac8c466db432155e454670deba95b64
trung-nt3 <trungqn.ptit@gmail.com>
2022-05-10 16:04:15 +0700

    init pystarport

---
+++ pystarport/default.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> {} }:
+let chain = import ../. { inherit pkgs; };
+in
+  pkgs.poetry2nix.mkPoetryApplication {
+    projectDir = ./.;
+    preBuild = ''
+    sed -i -e "s@CHAIN = 'chain-maind'  # edit by nix-build@CHAIN = '${chain}/bin/chain-maind'@" pystarport/cluster.py
+    '';
+  }
