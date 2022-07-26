0a0cb6260e4608ec75f76efdde2085e27cfe0321
hoanguyenkh <hoa.nguyen2@tiki.vn>
2022-07-26 09:11:15 +0700

    refactor: update upgrade info

--- app/upgrades/v1_1/constants.go
+++ app/upgrades/v1_1/constants.go
@@ -4,13 +4,13 @@ import "time"
 
 const (
 	// UpgradeName is the shared upgrade plan name for testnet
-	UpgradeName = "v1.0.1"
+	UpgradeName = "v1.1.0"
 	// TestUpgradeHeight defines the Evmos mainnet block height on which the upgrade will take place
-	TestUpgradeHeight = 253230 // (24 * 60 * 60) / 2.6 + 220000
+	TestUpgradeHeight = 300000 // (24 * 60 * 60) / 2.6 + 260000
 	// UpgradeInfo defines the binaries that will be used for the upgrade
-	UpgradeInfo = ``
+	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/astraProtocol/astra/releases/download/v1.1.0/astra_1.1.0_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/astraProtocol/astra/releases/download/v1.1.0/astra_1.1.0_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/astraProtocol/astra/releases/download/v1.1.0/astra_1.1.0_Linux_arm64.tar.gz","linux/x86_64":"https://github.com/astraProtocol/astra/releases/download/v1.1.0/astra_1.1.0_Linux_x86_64.tar.gz","windows/x86_64":"https://github.com/astraProtocol/astra/releases/download/v1.1.0/astra_1.1.0_Windows_x86_64.zip"}}'`
 
 	AvgBlockTime = 3 * time.Second
 
-	NewMaxGas = 40_000_000
+	NewMaxGas = int64(40_000_000)
 )
