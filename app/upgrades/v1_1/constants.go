faf1d039afa694acc7f902174f5fa26f5556ed30
hoanguyenkh <hoa.nguyen2@tiki.vn>
2022-07-28 09:19:57 +0700

    refactor: update plan

--- app/upgrades/v1_1/constants.go
+++ app/upgrades/v1_1/constants.go
@@ -6,7 +6,7 @@ const (
 	// UpgradeName is the shared upgrade plan name for testnet
 	UpgradeName = "v1.1.0"
 	// TestUpgradeHeight defines the Evmos mainnet block height on which the upgrade will take place
-	TestUpgradeHeight = 350000
+	TestUpgradeHeight = 400000
 	// UpgradeInfo defines the binaries that will be used for the upgrade
 	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/astraProtocol/astra/releases/download/v1.1.0/astra_1.1.0_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/astraProtocol/astra/releases/download/v1.1.0/astra_1.1.0_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/astraProtocol/astra/releases/download/v1.1.0/astra_1.1.0_Linux_arm64.tar.gz","linux/x86_64":"https://github.com/astraProtocol/astra/releases/download/v1.1.0/astra_1.1.0_Linux_x86_64.tar.gz","windows/x86_64":"https://github.com/astraProtocol/astra/releases/download/v1.1.0/astra_1.1.0_Windows_x86_64.zip"}}'`
 
