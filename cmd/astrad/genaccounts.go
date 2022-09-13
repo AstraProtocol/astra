6ae4aff30ad3011ed43f153ce189122b2f5c21d6
hoanguyenkh <hoa.nguyen2@tiki.vn>
2022-09-13 16:59:04 +0700

    fix: add miss flags

--- cmd/astrad/genaccounts.go
+++ cmd/astrad/genaccounts.go
@@ -314,7 +314,9 @@ contain valid denominations. Accounts may optionally be supplied with vesting pa
 
 	cmd.Flags().String(flags.FlagHome, defaultNodeHome, "The application home directory")
 	cmd.Flags().String(flags.FlagKeyringBackend, flags.DefaultKeyringBackend, "Select keyring's backend (os|file|kwallet|pass|test)")
+	cmd.Flags().String(flagVestingAmt, "", "amount of coins for vesting accounts")
 	cmd.Flags().Int64(flagVestingStart, 0, "schedule start time (unix epoch) for vesting accounts")
+	cmd.Flags().Int64(flagVestingEnd, 0, "schedule end time (unix epoch) for vesting accounts")
 	cmd.Flags().Bool(vestingcli.FlagClawback, false, "create clawback account")
 	cmd.Flags().String(vestingcli.FlagFunder, "", "funder address for clawback")
 	cmd.Flags().String(vestingcli.FlagLockup, "", "path to file containing unlocking periods")
