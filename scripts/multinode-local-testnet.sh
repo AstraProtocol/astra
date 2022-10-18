4a9b6ef2215766eabe8f4cf7bb875eeb723abd1c
hoanguyenkh <hoa.nguyen2@tiki.vn>
2022-10-18 20:51:07 +0700

    update testnet node

--- scripts/multinode-local-testnet.sh
+++ scripts/multinode-local-testnet.sh
@@ -26,9 +26,9 @@ sed -i -E 's|swagger = false|swagger = true|g' .testnets/node0/astrad/config/app
 # validator2
 sed -i -E 's|tcp://0.0.0.0:1317|tcp://0.0.0.0:1316|g' .testnets/node1/astrad/config/app.toml
 sed -i -E 's|0.0.0.0:9090|0.0.0.0:9088|g' .testnets/node1/astrad/config/app.toml
+sed -i -E 's|0.0.0.0:9091|0.0.0.0:9089|g' .testnets/node1/astrad/config/app.toml
 sed -i -E 's|0.0.0.0:8545|0.0.0.0:8547|g' .testnets/node1/astrad/config/app.toml
 sed -i -E 's|0.0.0.0:8546|0.0.0.0:8548|g' .testnets/node1/astrad/config/app.toml
-sed -i -E 's|0.0.0.0:9091|0.0.0.0:9089|g' .testnets/node1/astrad/config/app.toml
 sed -i -E 's|swagger = false|swagger = true|g' .testnets/node1/astrad/config/app.toml
 
 # validator3
@@ -38,29 +38,48 @@ sed -i -E 's|0.0.0.0:9091|0.0.0.0:9087|g' .testnets/node2/astrad/config/app.toml
 sed -i -E 's|0.0.0.0:8545|0.0.0.0:8549|g' .testnets/node2/astrad/config/app.toml
 sed -i -E 's|0.0.0.0:8546|0.0.0.0:8550|g' .testnets/node2/astrad/config/app.toml
 sed -i -E 's|swagger = false|swagger = true|g' .testnets/node2/astrad/config/app.toml
-astrad tendermint show-node-id --keyring-backend=test --home=.testnets/node2/astrad
+
+# validator4
+sed -i -E 's|tcp://0.0.0.0:1317|tcp://0.0.0.0:1314|g' .testnets/node3/astrad/config/app.toml
+sed -i -E 's|0.0.0.0:9090|0.0.0.0:9084|g' .testnets/node3/astrad/config/app.toml
+sed -i -E 's|0.0.0.0:9091|0.0.0.0:9085|g' .testnets/node3/astrad/config/app.toml
+sed -i -E 's|0.0.0.0:8545|0.0.0.0:8551|g' .testnets/node3/astrad/config/app.toml
+sed -i -E 's|0.0.0.0:8546|0.0.0.0:8552|g' .testnets/node3/astrad/config/app.toml
+sed -i -E 's|swagger = false|swagger = true|g' .testnets/node3/astrad/config/app.toml
 
 # change config.toml values
 
 # validator1
 sed -i -E 's|allow_duplicate_ip = false|allow_duplicate_ip = true|g' .testnets/node0/astrad/config/config.toml
-sed -i -E 's|tcp://0.0.0.0:40002|tcp://0.0.0.0:40000|g' .testnets/node0/astrad/config/config.toml
-sed -i -E 's|tcp://0.0.0.0:50002|tcp://0.0.0.0:50000|g' .testnets/node0/astrad/config/config.toml
+sed -i -E 's|tcp://0.0.0.0:40003|tcp://0.0.0.0:40000|g' .testnets/node0/astrad/config/config.toml
+sed -i -E 's|tcp://0.0.0.0:50003|tcp://0.0.0.0:50000|g' .testnets/node0/astrad/config/config.toml
 # validator2
 sed -i -E 's|tcp://127.0.0.1:26658|tcp://127.0.0.1:26655|g' .testnets/node1/astrad/config/config.toml
 sed -i -E 's|allow_duplicate_ip = false|allow_duplicate_ip = true|g' .testnets/node1/astrad/config/config.toml
 # validator3
-sed -i -E 's|tcp://127.0.0.1:26658|tcp://127.0.0.1:26652|g' .testnets/node2/astrad/config/config.toml
-sed -i -E 's|tcp://0.0.0.0:40002|tcp://0.0.0.0:40001|g' .testnets/node1/astrad/config/config.toml
-sed -i -E 's|tcp://0.0.0.0:50002|tcp://0.0.0.0:50001|g' .testnets/node1/astrad/config/config.toml
+sed -i -E 's|tcp://0.0.0.0:40003|tcp://0.0.0.0:40001|g' .testnets/node1/astrad/config/config.toml
+sed -i -E 's|tcp://0.0.0.0:50003|tcp://0.0.0.0:50001|g' .testnets/node1/astrad/config/config.toml
 
+sed -i -E 's|tcp://127.0.0.1:26658|tcp://127.0.0.1:26652|g' .testnets/node2/astrad/config/config.toml
 sed -i -E 's|allow_duplicate_ip = false|allow_duplicate_ip = true|g' .testnets/node2/astrad/config/config.toml
+sed -i -E 's|tcp://0.0.0.0:40003|tcp://0.0.0.0:40002|g' .testnets/node2/astrad/config/config.toml
+sed -i -E 's|tcp://0.0.0.0:50003|tcp://0.0.0.0:50002|g' .testnets/node2/astrad/config/config.toml
+
+# validator 4
+sed -i -E 's|tcp://127.0.0.1:26658|tcp://127.0.0.1:26651|g' .testnets/node3/astrad/config/config.toml
+sed -i -E 's|allow_duplicate_ip = false|allow_duplicate_ip = true|g' .testnets/node3/astrad/config/config.toml
+
+sed -i -E 's|timeout_commit = "5s"|timeout_commit = "1s"|g' .testnets/node0/astrad/config/config.toml
+sed -i -E 's|timeout_commit = "5s"|timeout_commit = "1s"|g' .testnets/node1/astrad/config/config.toml
+sed -i -E 's|timeout_commit = "5s"|timeout_commit = "1s"|g' .testnets/node2/astrad/config/config.toml
+sed -i -E 's|timeout_commit = "5s"|timeout_commit = "1s"|g' .testnets/node3/astrad/config/config.toml
 
 # copy validator1 genesis file to validator2-3
 cp .testnets/node0/astrad/config/genesis.json .testnets/node1/astrad/config/genesis.json
 cp .testnets/node0/astrad/config/genesis.json .testnets/node2/astrad/config/genesis.json
+cp .testnets/node0/astrad/config/genesis.json .testnets/node3/astrad/config/genesis.json
 
-echo "start all three validators"
+echo "start all four validators"
 #screen -S validator1 -d -m astrad start --home=.testnets/node0/astrad
 #screen -S validator2 -d -m astrad start --home=.testnets/node1/astrad
 # screen -S validator3 -d -m astrad start --home=.testnets/node2/astrad
