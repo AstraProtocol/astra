541c8b211311c9a9b001417cdca755c9530fd467
LampardNguyen234 <thanh230497@gmail.com>
2022-12-23 10:14:55 +0700

    feat: add mint types

---
+++ x/mint/types/keys.go
@@ -0,0 +1,26 @@
+package types
+
+const (
+	ModuleName = "mint"
+
+	// StoreKey is the default store key for mint
+	StoreKey = ModuleName
+
+	// QuerierRoute is the querier route for the minting store.
+	QuerierRoute = StoreKey
+
+	QueryParameters       = "parameters"
+	QueryInflation        = "inflation"
+	QueryAnnualProvisions = "annual_provisions"
+)
+
+const (
+	prefixMinter = iota
+	prefixTotalMintedProvision
+)
+
+// MinterKey is the key to use for the keeper store.
+var (
+	MinterKey                     = []byte{prefixMinter}
+	KeyPrefixTotalMintedProvision = []byte{prefixTotalMintedProvision}
+)
