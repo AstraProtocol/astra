90af2381312af18299fa00ca35116501b7fdb025
hoanguyenkh <hoa.nguyen2@tiki.vn>
2022-12-23 16:41:19 +0700

    add test send batch transaction

--- x/feeburn/keeper/integration_test.go
+++ x/feeburn/keeper/integration_test.go
@@ -8,15 +8,22 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/tx/signing"
 	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/evmos/ethermint/crypto/ethsecp256k1"
 	"github.com/evmos/ethermint/encoding"
+	"github.com/evmos/ethermint/tests"
+	evmtypes "github.com/evmos/ethermint/x/evm/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 	"math"
+	"math/big"
 )
 
 var (
 	DEFAULT_FEE int64 = 1000000
+	privs       []*ethsecp256k1.PrivKey
 )
 
 func (suite *KeeperTestSuite) TestBurnFeeCosmosTxDelegate() {
@@ -32,15 +39,67 @@ func (suite *KeeperTestSuite) TestBurnFeeCosmosTxDelegate() {
 	delegateAmount := sdk.NewCoin(config.BaseDenom, sdk.NewInt(1))
 	delegate(priv0, delegateAmount)
 	s.Commit()
-	mintParams := suite.app.MintKeeper.GetParams(suite.ctx)
-	mintedCoin := suite.app.MintKeeper.GetMinter(suite.ctx).BlockProvision(mintParams)
+	mintedCoin := getMintedCoin()
 	totalSupplyAfter := suite.app.BankKeeper.GetSupply(suite.ctx, config.BaseDenom)
 	fmt.Println("totalSupplyAfter", totalSupplyAfter)
-	feeBurnParams := suite.app.FeeBurnKeeper.GetParams(s.ctx)
 	expectAmount := totalSupplyAfter.Amount.Sub(totalSupplyBefore.Amount)
 	expectAmount = mintedCoin.Amount.Sub(expectAmount)
 	fmt.Println("expectAmount", expectAmount)
-	s.Require().Equal(feeBurnParams.FeeBurn.MulInt(sdk.NewInt(DEFAULT_FEE)).RoundInt(), expectAmount)
+	s.Require().Equal(getExpectTotalFeeBurn(1), expectAmount)
+}
+
+func (suite *KeeperTestSuite) TestBurnFeeCosmosTxSend() {
+	suite.SetupTest()
+	accountCount := 50
+	for i := 0; i < accountCount; i++ {
+		priv, _ := ethsecp256k1.GenerateKey()
+		privs = append(privs, priv)
+		addr := getAddr(priv)
+		accBalance := sdk.Coins{{Denom: config.BaseDenom, Amount: sdk.NewInt(int64(math.Pow10(18) * 2))}}
+		err := suite.FundAccount(suite.ctx, addr, accBalance)
+		s.Require().NoError(err)
+	}
+	s.Commit()
+	totalSupplyBefore := suite.app.BankKeeper.GetSupply(suite.ctx, config.BaseDenom)
+	fmt.Println("totalSupply", totalSupplyBefore)
+	sendAmount := sdk.NewCoin(config.BaseDenom, sdk.NewInt(10))
+	for i := 0; i < accountCount; i++ {
+		send(privs[i], sendAmount)
+	}
+	s.Commit()
+
+	totalSupplyAfter := suite.app.BankKeeper.GetSupply(suite.ctx, config.BaseDenom)
+	fmt.Println("totalSupplyAfter", totalSupplyAfter)
+	expectAmount := totalSupplyAfter.Amount.Sub(totalSupplyBefore.Amount)
+	mintedCoin := getMintedCoin()
+	expectAmount = mintedCoin.Amount.Sub(expectAmount)
+	fmt.Println("expectAmount", expectAmount)
+	s.Require().Equal(getExpectTotalFeeBurn(accountCount), expectAmount)
+	s.Commit()
+	totalSupplyAfter1 := suite.app.BankKeeper.GetSupply(suite.ctx, config.BaseDenom)
+	fmt.Println("totalSupplyAfter1", totalSupplyAfter1)
+
+	s.Require().Equal(getMintedCoin(), totalSupplyAfter1.Sub(totalSupplyAfter))
+}
+
+func (suite *KeeperTestSuite) TestEvmTx() {
+	suite.SetupTest()
+	priv, _ := ethsecp256k1.GenerateKey()
+	accBalance := sdk.Coins{{Denom: config.BaseDenom, Amount: sdk.NewInt(int64(math.Pow10(18) * 2))}}
+	addr := getAddr(priv)
+	err := suite.FundAccount(suite.ctx, addr, accBalance)
+	s.Require().NoError(err)
+	sendEthToSelf(priv)
+}
+
+func getExpectTotalFeeBurn(numberTx int) sdk.Int {
+	feeBurnParams := s.app.FeeBurnKeeper.GetParams(s.ctx)
+	return feeBurnParams.FeeBurn.MulInt(sdk.NewInt(DEFAULT_FEE).Mul(sdk.NewInt(int64(numberTx)))).RoundInt()
+}
+
+func getMintedCoin() sdk.Coin {
+	mintParams := s.app.MintKeeper.GetParams(s.ctx)
+	return s.app.MintKeeper.GetMinter(s.ctx).BlockProvision(mintParams)
 }
 
 func getAddr(priv *ethsecp256k1.PrivKey) sdk.AccAddress {
@@ -55,6 +114,16 @@ func delegate(priv *ethsecp256k1.PrivKey, delegateAmount sdk.Coin) {
 
 	delegateMsg := stakingtypes.NewMsgDelegate(accountAddress, val, delegateAmount)
 	deliverTx(priv, delegateMsg)
+}
+
+func send(priv *ethsecp256k1.PrivKey, sendAmount sdk.Coin) {
+	accountAddress := sdk.AccAddress(priv.PubKey().Address().Bytes())
+	privNew, _ := ethsecp256k1.GenerateKey()
+	addrRecv := getAddr(privNew)
+
+	amount := sdk.Coins{sendAmount}
+	sendMsg := banktypes.NewMsgSend(accountAddress, addrRecv, amount)
+	deliverTx(priv, sendMsg, sendMsg)
 }
 
 func prepareCosmosTx(priv *ethsecp256k1.PrivKey, msgs ...sdk.Msg) []byte {
@@ -111,6 +180,39 @@ func prepareCosmosTx(priv *ethsecp256k1.PrivKey, msgs ...sdk.Msg) []byte {
 	bz, err := encodingConfig.TxConfig.TxEncoder()(txBuilder.GetTx())
 	s.Require().NoError(err)
 	return bz
+}
+
+func sendEthToSelf(priv *ethsecp256k1.PrivKey) {
+	chainID := s.app.EvmKeeper.ChainID()
+	from := common.BytesToAddress(priv.PubKey().Address().Bytes())
+	nonce := s.app.EvmKeeper.GetNonce(s.ctx, from)
+
+	msgEthereumTx := evmtypes.NewTx(chainID, nonce, &from, nil, 100000, nil,
+		s.app.FeeMarketKeeper.GetBaseFee(s.ctx), big.NewInt(1), nil, &ethtypes.AccessList{})
+	msgEthereumTx.From = from.String()
+	performEthTx(priv, msgEthereumTx)
+}
+
+func performEthTx(priv *ethsecp256k1.PrivKey, msgEthereumTx *evmtypes.MsgEthereumTx) {
+	// Sign transaction
+	err := msgEthereumTx.Sign(s.ethSigner, tests.NewSigner(priv))
+	s.Require().NoError(err)
+
+	// Assemble transaction from fields
+	encodingConfig := encoding.MakeConfig(app.ModuleBasics)
+	txBuilder := encodingConfig.TxConfig.NewTxBuilder()
+	tx, err := msgEthereumTx.BuildTx(txBuilder, config.BaseDenom)
+	s.Require().NoError(err)
+
+	// Encode transaction by default Tx encoder and broadcasted over the network
+	txEncoder := encodingConfig.TxConfig.TxEncoder()
+	bz, err := txEncoder(tx)
+	s.Require().NoError(err)
+
+	req := abci.RequestDeliverTx{Tx: bz}
+	res := s.app.BaseApp.DeliverTx(req)
+	fmt.Println(res)
+	s.Require().Equal(true, res.IsOK())
 }
 
 func deliverTx(priv *ethsecp256k1.PrivKey, msgs ...sdk.Msg) abci.ResponseDeliverTx {
