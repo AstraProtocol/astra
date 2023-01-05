6db8239a510582f5dc4a5a3506e56a9b023331cb
Khanh Hoa <49144992+hoanguyenkh@users.noreply.github.com>
2023-01-05 13:45:40 +0700

    refactor: add handler errror (#160)

--- x/feeburn/module.go
+++ x/feeburn/module.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+
 	"github.com/AstraProtocol/astra/v2/x/feeburn/client/rest"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 
@@ -74,7 +75,9 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
-	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
+	if err := types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx)); err != nil {
+		panic(err)
+	}
 }
 
 // GetTxCmd returns the capability module's root tx command.
