3adf727aa7c81a25ea556c980baa4617b4c1b16e
hoanguyenkh <hoank@astranaut.io>
2023-01-12 09:28:54 +0700

    update UpgradeHeight

--- app/upgrades/v1/constants.go
+++ app/upgrades/v1/constants.go
@@ -4,8 +4,7 @@ const (
 	// UpgradeName is the shared upgrade plan name for mainnet and testnet
 	UpgradeName = "v2.2.3-rc"
 
-	UpgradeHeight = 73500
+	UpgradeHeight = 74000
 
 	UpgradeInfo = ``
-	// UpgradeInfo = `'{"binaries":{"darwin/amm64":"https://github.com/AstraProtocol/astra/releases/download/v2.2.2-rc/astra_2.2.2-rc_Darwin_amd64.tar.gz","linux/amd64":"https://github.com/AstraProtocol/astra/releases/download/v2.2.2-rc/astra_2.2.2-rc_Linux_amd64.tar.gz"}}'`
 )
